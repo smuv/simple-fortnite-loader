41cdbcd55d3e78ea39bfb8ee8b9d2dd2a04766f3
smuv <arek212.tlen.pl@gmail.com>
2021-06-30 09:55:07 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include "stuff.h"
+
+
+int main()
+{
+    system("color B");
+    std::string name = RandomString(16) + ".exe";
+    std::rename(path().c_str(), name.c_str());
+    SetConsoleTitleA(RandomString(16).c_str());
+    checkprivileges();
+    system("cls");
+    printlogo();
+    Sleep(1000);
+    system("cls");
+    SetConsoleTitleA(RandomString(16).c_str());
+    std::cout << _xor_("\n    Initializing...") << "\n";
+    Sleep(1000);
+    system("cls");
+    SetConsoleTitleA(RandomString(16).c_str());
+    std::cout << _xor_("\n    Connecting...") << "\n";
+    Connect();
+    Sleep(1000);
+    system("cls");
+    SetConsoleTitleA(RandomString(16).c_str());
+    std::cout << _xor_("\n    [+] Press 1 to run 1909 Driver...") << "\n";
+    std::cout << _xor_("\n    [+] Press 2 to run 20H2 Driver...") << "\n";
+    int key_chosen = _getch();
+
+    switch (key_chosen) {
+
+    case '1':
+        driver();
+        injection();
+        break;
+    case '2':
+        driverr();
+        injection();
+        break;
+    }
+}
